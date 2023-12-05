bf29cd8e3448daadc45fb0bc26d6b90a875b16f9
Lajos Nagy <nagylali001@gmail.com>
2023-12-05 20:12:39 +0100

    Add docstring to temp_functions

--- L4/temp_functions.py
+++ L4/temp_functions.py
@@ -1,8 +1,18 @@
 def fahr_to_celsius(temp_fahrenheit):
+    '''
+    Convert temperatures from Fahrenheit to Celsius.
+    '''
     return (temp_fahrenheit - 32) / 1.8
 
 
 def temp_classifier(temp_celsius):
+    '''
+    Classify given temperatures based on the criteria below:
+    | 0            | Temperatures below -2 degrees Celsius                                    |
+    | 1            | Temperatures equal or warmer than -2, but less than +2 degrees Celsius   |
+    | 2            | Temperatures equal or warmer than +2, but less than +15 degrees Celsius  |
+    | 3            | Temperatures equal or warmer than +15 degrees Celsius                    | 
+    '''
     if temp_celsius < -2:
         return 0
     elif -2 <= temp_celsius < 2:
